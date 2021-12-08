2f6b923b1d51862cbbb59cbdfda4e761a80c232e
Zvonimir Mesinger <zvonimir.mesinger@gmail.com>
2021-12-08 16:51:58 +0100

    Add single chord html and css

--- controller/index.js
+++ controller/index.js
@@ -1,7 +1,22 @@
 function linkToRegisterForm(){
-    window.location='../view/login_register.html';
+    window.location='../view/register.html';
 }
 
 function linkToAllChords(){
     window.location='../view/all_chords.html';
 }
+
+function checkIfEmail(){
+    var email = document.getElementById('email');
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if(email.value.match(mailformat)){
+
+        return true;
+        
+    }else{
+        alert("Invalid email");
+        return false;
+    }
+}
+
